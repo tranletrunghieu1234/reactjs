0955da2e5a4562b443efcc609555cfd132cd7630
tranletrunghieu1234 <tranletrunghieu1234@gmail.com>
2020-07-14 12:39:45 +0700

    reactjs

---
+++ src/Demo.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Demo() {
+  return (
+    <div>
+      <h2>Hello Reactjs</h2>
+      <p> Chào mừng đến với khóa học lập trình reatctjs</p>
+    </div>
+  );
+}
+
+export default Demo;
